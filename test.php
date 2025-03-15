b6cd78be0a720900dc6c3022e4dc12c722e936c6
Ortiss04 <vangduythuan.nkkn2020@gmail.com>
2025-03-15 21:28:21 +0700

    chinh max width

--- test.php
+++ test.php
@@ -1,4 +1,20 @@
-<?php
-
-echo "Test github";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-md">
+        <h1 class="text-center text-primary">Hiiii sử dụng Bootstrap thành công!</h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium numquam aliquid officia, 
+        quas sequi amet accusamus nisi quae nihil quibusdam ullam iusto dicta suscipit unde labore adipisci
+         distinctio recusandae ex?
+        <button class="btn btn-success">Nhấn vào đây</button>
+    </div>
+</body>
+</html>
